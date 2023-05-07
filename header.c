8c763ed01a374cc3589a72a5c2d54e6721376833
boochow <boochowp@gmail.com>
2023-05-08 08:07:16 +0900

    remove dummy parameters

--- header.c
+++ header.c
@@ -26,25 +26,14 @@ const __unit_header unit_header_t unit_header = {
         // See common/runtime.h for type enum and unit_param_t structure
 
         // Page 1
-        // percent param with .5 precision e.g.: "25.0%", "50.5%"
-        {0, (100 << 1), 0, (25 << 1), k_unit_param_type_percent, 1, 0, 0, {"PARAM1"}},
-        // untyped bipolar parameter centered at 0 e.g.: "-5", "0", "2"
-        {-5, 5, 0, 0, k_unit_param_type_none, 0, 0, 0, {"PARAM2"}},
-        // pan parameter e.g.: "L50", "C", "R100"
-        {-100, 100, 0, 0, k_unit_param_type_pan, 0, 0, 0, {"PARAM3"}},
-        // blank parameter, leaves that parameter slot blank in UI. Use when
-        // want to align some params to next page
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
 
         // Page 2
-        // string enum parameter, unit_get_param_str_value will be called with
-        // numerical value to obtain string to display
-        {0, 9, 0, 0, k_unit_param_type_strings, 0, 0, 0, {"PARAM4"}},
-        // bitmap enum parameter, unit_get_param_bmp_value will be called with
-        // numerical value to obtain the bitmap to display
-        // Note: bitmap specifications not final yet, and not implemented yet in
-        // UI
-        {0, 9, 0, 0, k_unit_param_type_bitmaps, 0, 0, 0, {"PARAM5"}},
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
 
